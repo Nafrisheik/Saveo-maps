32d601f357eea2fd979ac1ced561994844e3c64e
Irfan <you@example.com>
2021-03-03 04:52:00 +0530

    removed color

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render (
   <React.StrictMode>
-    <App  style={{background:'#72A1BF'}} />
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
